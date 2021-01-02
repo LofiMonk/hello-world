6bee59cf9b026910dd87a5d29f317b6adcde8872
drewcifer <a.wagenschutz@gmail.com>
2021-01-02 12:01:10 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,26 @@
+//normal
+//! important error
+//? nice
+//* highlighted
+//TODO
+
+/**
+ * normal
+ * ! fancy
+ * ? colors
+ * * highlight
+ * TODO
+ * @param myParam  description of the parameter or method
+ *
+ * multi line comments
+ *
+ */
+
+var test = 10;
+console.log(test);
+
+function myFunct(lame) {
+  if (lame < 20) {
+    console.log("good job");
+  }
+}
